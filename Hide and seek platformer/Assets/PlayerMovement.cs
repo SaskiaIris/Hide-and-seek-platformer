a13b579055f26c5ca32e7f11a862b5592a839f76
IrisSaskia <iris.oostra@hva.nl>
2019-10-15 18:06:44 +0200

    Het poppetje loopt een soort van
    Ze doet ook backflips enzo.

--- Hide and seek platformer/Assets/PlayerMovement.cs
+++ Hide and seek platformer/Assets/PlayerMovement.cs
@@ -4,19 +4,33 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+	private float movement;
+	private Vector2 velocity;
+	private Rigidbody2D m_Rigidbody2D;
+	private Vector3 m_Velocity = Vector3.zero;
+	[Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Input.
+        movement = Input.GetAxis("Horizontal");
     }
 
-    void FixedUpdate() {
+    void FixedUpdate() 
+    {
+    	if(movement != 0) {
+    		velocity = new Vector2(movement, 0);
+    	}
 
+			Vector3 targetVelocity = new Vector2(movement * 10f, m_Rigidbody2D.velocity.y);
+			// And then smoothing it out and applying it to the character
+			m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+    	//m_Rigidbody2D.AddForce(velocity);
     }
 }
